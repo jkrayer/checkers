7df2a3243eb668c4fd1e9442f94e2c96a59b232f
Jim Krayer <jameskrayer@yahoo.com>
2021-12-02 16:53:30 -0500

    Revert "DRY out the server"
    This reverts commit 441db3684b1a4fc8a970c1e8c8904d6d88e84aaa.

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
 import http from "http";
 import path from "path";
 import fs from "fs";
-import { getContentType } from "./lib/index.js";
-// import { hasGame } from "./lib/game.js";
+import { getImageType } from "./lib/index.js";
+import { hasGame } from "./lib/game.js";
 
 const PORT = 8080;
-const resource = /jpg|png|gif|js|css$/;
 
 const server = http.createServer(async (req, res) => {
   const file = path.resolve(`./public${req.url}`);
 
-  if (resource.test(file)) {
+  if (/\.jpg|png|gif$/.test(req.url)) {
+    const imgType = getImageType(req.url);
+
     fs.open(file, (err) => {
       if (err === null) {
-        res.writeHead(200, {
-          "Content-Type": getContentType(path.extname(file)),
-        });
+        res.writeHead(200, { "Content-Type": `image/${imgType}` });
         fs.createReadStream(file, "utf-8").pipe(res);
       } else {
         res.writeHead(404);
         res.end();
       }
     });
+  } else if (/\.js$/.test(req.url)) {
+    fs.open(file, (err) => {
+      if (err === null) {
+        res.writeHead(200, { "Content-Type": "application/javascript" });
+        fs.createReadStream(file, "utf-8").pipe(res);
+      } else {
+        res.writeHead(404);
+        res.end();
+      }
+    });
+  } else if (/game\/[a-zA-Z0-9]{5,5}/.test(req.url)) {
+    const gameId = req.url.slice(-5);
+
+    if (hasGame(gameId)) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ game: "g" }));
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
   } else {
     res.writeHead(200, { "Content-Type": "text/html" });
     fs.createReadStream(path.resolve("./public/index.html"), "utf-8").pipe(res);
@@ -30,15 +49,3 @@ const server = http.createServer(async (req, res) => {
 
 server.listen(PORT, "127.0.0.1");
 console.log("Server listening to ", PORT);
-
-// else if (/game\/[a-zA-Z0-9]{5,5}/.test(req.url)) {
-//     const gameId = req.url.slice(-5);
-
-//     if (hasGame(gameId)) {
-//       res.writeHead(200, { "Content-Type": "application/json" });
-//       res.end(JSON.stringify({ game: "g" }));
-//     } else {
-//       res.writeHead(404);
-//       res.end();
-//     }
-//   }
